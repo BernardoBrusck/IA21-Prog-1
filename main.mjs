f9fefa7969d58743431e3cf7c06a6ed230594873
BRUSCK <104568825+BernardoBrusck@users.noreply.github.com>
2022-05-05 14:22:13 -0300

    questão 1

---
+++ main.mjs
@@ -0,0 +1,21 @@
+import * as readline from 'readline'
+import { stdin as input, stdout as output } from 'process'
+const rl = readline.createInterface({ input, output })
+const question = q => new Promise((rs, rj) => rl.question(q + ' ', a => rs(a)))
+
+let cont = 0
+let media = 0
+let soma = 0
+
+while(true){
+    let x = await question('Digite um número: ')
+    if (x == 'n') break
+
+    let notas = parseFloat(x)
+
+    cont++
+    soma = soma + notas
+}
+
+media = soma / cont
+console.log("Média = ", media)
